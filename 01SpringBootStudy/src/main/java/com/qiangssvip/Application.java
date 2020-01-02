e5e0b095d00e0e371d61794d49902411fa99dc7c
xinggevip <1511844263@qq.com>
2020-01-02 09:56:36 +0800

    SpringBoot快速入门

---
+++ 01SpringBootStudy/src/main/java/com/qiangssvip/Application.java
@@ -0,0 +1,11 @@
+package com.qiangssvip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
