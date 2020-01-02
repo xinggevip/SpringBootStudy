0e2a5e2ef7cf2f00fa5cbc1e7c1a0d3c96e59d17
xinggevip <1511844263@qq.com>
2020-01-02 18:30:10 +0800

    SpringBootSSM-日志级别设置

--- 03SpringBootSSMStudy/src/main/java/com/qiangssvip/web/MyController.java
+++ 03SpringBootSSMStudy/src/main/java/com/qiangssvip/web/MyController.java
@@ -1,13 +1,23 @@
 package com.qiangssvip.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 
+    private static final Logger log = LoggerFactory.getLogger(MyController.class);
+
     @RequestMapping("/hello")
     public String hello(){
+        /**
+         * debug级别既输出info又输出debug
+         * info级别只输出info
+         * */
+        log.info("输出日志====info");
+        log.debug("输出日志====debug");
         return "Hellow SpringBootSSM";
     }
 }
