804b76b439a5d8fbaac4951eee545c376f32c0de
xinggevip <1511844263@qq.com>
2020-01-02 18:15:18 +0800

    SpringBootSSM-静态资源访问

---
+++ 03SpringBootSSMStudy/src/main/java/com/qiangssvip/web/MyController.java
@@ -0,0 +1,13 @@
+package com.qiangssvip.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hellow SpringBootSSM";
+    }
+}
