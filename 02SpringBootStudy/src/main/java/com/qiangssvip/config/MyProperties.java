816aa985fde4af90fa893c688b9ec2c768e22c5e
xinggevip <1511844263@qq.com>
2020-01-02 17:16:17 +0800

    属性配置yam自定义属性和值

--- 02SpringBootStudy/src/main/java/com/qiangssvip/config/MyProperties.java
+++ 02SpringBootStudy/src/main/java/com/qiangssvip/config/MyProperties.java
@@ -4,6 +4,9 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
+
 @Component
 @ConfigurationProperties(prefix = "jdbc")
 @Data
@@ -15,4 +18,12 @@ public class MyProperties {
     private String username;
 
     private String password;
+
+    private String[] arrayPros;
+
+    private List<String> listPros;
+
+    private Map<String,String> mapPros;
+
+    private List<Map<String,String>> listMapPros;
 }
