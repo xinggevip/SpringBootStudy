437ce8b05845231787478ee2b59967724254aaed
xinggevip <1511844263@qq.com>
2020-01-03 12:20:00 +0800

    SpringBootSSM-配置拦截器

---
+++ 03SpringBootSSMStudy/src/main/java/com/qiangssvip/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.qiangssvip.config;
+
+import com.qiangssvip.interceptor.MyInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    private MyInterceptor myInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(myInterceptor).addPathPatterns("/**");
+    }
+}
---
+++ 03SpringBootSSMStudy/src/main/java/com/qiangssvip/interceptor/MyInterceptor.java
@@ -0,0 +1,24 @@
+package com.qiangssvip.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@Component
+@Slf4j
+public class MyInterceptor implements HandlerInterceptor {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.debug("MyInterceptor------preHandle");
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+    }
+
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+    }
+}
