e1d8de588dc918faa34f6aff65b41614b20e7e85
xinggevip <1511844263@qq.com>
2020-01-03 10:26:41 +0800

    SpringBootSSM-日志输出到文件夹

--- 03SpringBootSSMStudy/src/main/java/com/qiangssvip/web/MyController.java
+++ 03SpringBootSSMStudy/src/main/java/com/qiangssvip/web/MyController.java
@@ -1,14 +1,15 @@
 package com.qiangssvip.web;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class MyController {
 
-    private static final Logger log = LoggerFactory.getLogger(MyController.class);
+    /* 可以省略不写，加上@Slf4j注解即可 */
+    // private static final Logger log = LoggerFactory.getLogger(MyController.class);
 
     @RequestMapping("/hello")
     public String hello(){
